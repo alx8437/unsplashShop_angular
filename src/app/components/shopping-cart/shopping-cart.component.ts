112ce089ba06728fc451c9913f4de48573f34863
Aleksandr Zelenskiy <dezion777@gmail.com>
2020-07-29 19:10:15 +0300

    learn BehaviorSubject for CartComponent

--- src/app/components/shopping-cart/shopping-cart.component.ts
+++ src/app/components/shopping-cart/shopping-cart.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-shopping-cart',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-cart.component.scss']
 })
 export class ShoppingCartComponent implements OnInit {
+
 
   constructor() { }
 
